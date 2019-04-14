da8b6179e94f9b799b7378b71b44e937caa2babf
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2019-04-15 01:13:57 +0200

    Refactoring, split main method

--- src/extension.ts
+++ src/extension.ts
@@ -7,14 +7,26 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 async function addReference(uri: vscode.Uri) {
-	const csprojs = (await vscode.workspace.findFiles('**/*.csproj'))
-		.map(c => c.fsPath)
-		.filter(c => c !== uri.fsPath);
-
+	const csprojs = await getOtherCsprojs(uri);
 	if (csprojs.length === 0) {
 		return;
 	}
 
+	const projectReferences = await showQuickPick(csprojs);
+	if (!projectReferences) {
+		return;
+	}
+
+	execCliCommand(uri.fsPath, projectReferences);
+}
+
+async function getOtherCsprojs(uri: vscode.Uri) {
+	return (await vscode.workspace.findFiles('**/*.csproj'))
+		.map(c => c.fsPath)
+		.filter(c => c !== uri.fsPath);
+}
+
+async function showQuickPick(csprojs: string[]) {
 	const picks = csprojs.map<vscode.QuickPickItem>(c => {
 		return {
 			label: path.basename(c, path.extname(c)),
@@ -27,9 +39,13 @@ async function addReference(uri: vscode.Uri) {
 		return;
 	}
 
-	const target = selection.map(p => `"${p.detail}"`).join(' ');
+	return selection.map(s => s.detail as string);
+}
 
-	cp.exec(`dotnet add "${uri.fsPath}" reference ${target}`, (err, stdout, stderr) => {
+function execCliCommand(project: string, projectReferences: string[]) {
+	const target = projectReferences.map(t => `"${t}"`).join(' ');
+
+	cp.exec(`dotnet add "${project}" reference ${target}`, (err, stdout, stderr) => {
 		if (stdout) {
 			vscode.window.showInformationMessage(stdout);
 		}
