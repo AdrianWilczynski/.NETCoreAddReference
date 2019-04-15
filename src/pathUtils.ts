bcdf94f9b12b802959a94fe91f94cc751738b648
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2019-04-15 23:11:25 +0200

    Check if path is absolute before resolving

--- src/pathUtils.ts
+++ src/pathUtils.ts
@@ -1,5 +1,7 @@
 import * as path from 'path';
 
 export function toAbsolutePaths(paths: string[], relativeTo: string) {
-    return paths.map(p => path.resolve(path.dirname(relativeTo), p));
+    return paths.map(p => !path.isAbsolute(p)
+        ? path.resolve(path.dirname(relativeTo), p)
+        : p);
 }
